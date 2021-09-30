5084f33f7236de565c4dc5d2502f9a41702aef53
koti-muddana <91659323+koti-muddana@users.noreply.github.com>
2021-09-30 13:48:18 +0530

    Add files via upload

---
+++ arrayMethod.js
@@ -0,0 +1,10 @@
+const values = [77,56,89,45,39,73,92,52,33,86]
+console.log(values.length);
+console.log(values);
+console.log(values.unshift(88));
+console.log(values);
+console.log(values.splice(2,3,55,88,44));
+console.log(values);
+console.log(values.shift());
+console.log(values);
+
