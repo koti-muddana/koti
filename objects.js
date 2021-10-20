b24e1d75f0294c19987daaa8271bd6c5bf00aeb8
koti-muddana <91659323+koti-muddana@users.noreply.github.com>
2021-10-20 15:02:36 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,8 @@
+let area = {
+    Name : "sampath",
+    Age : 27,
+    Location : "hyderabad"
+}
+console.log(`${area.Name} is ${area.Age} and lives in ${area.Location}.... `)
+area.Age = area.Age+1;
+console.log(`${area.Name} is ${area.Age} and lives in ${area.Location}.... `)
